33751cb1e9e5200cb7b71b7867df951923249d72
JonasDreessen <dreessenjonas95@gmail.com>
2019-06-25 16:24:07 +0200

    add the finder of the childeren class of card

--- js/main.js
+++ js/main.js
@@ -6,16 +6,23 @@ var clickCounter = 0;
 
 function flipCardToBackSide() {
     // Add the class to flip the card so the bgpicture is visible.
+    /* TRYOUT 
     this.classList.add("flip-card-animation");
+    var clickedCards = document.querySelectorAll(".flip-card-animation"); */
+
     clickCounter++;
 
     if (clickCounter === 1) {
-
-        console.log("clickcounter is 1");
+        this.classList.add("flip-card-animation");
+        var clickedCards = document.querySelectorAll(".flip-card-animation");
+        // Here It will get the second class of the second child of the .flip-card-animation class.
+        clickedCards.forEach(card => console.log(card.children[1].classList[1]));
 
     } else if (clickCounter === 2) {
+        this.classList.add("flip-card-animation2");
+        var clickedCards2 = document.querySelectorAll(".flip-card-animation2");
 
-        console.log("clickcounter is 2");
+        clickedCards2.forEach(card2 => console.log(card2.children[1].classList[1]));
         clickCounter = 0;
     }
 };
